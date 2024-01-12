e8cadd069807e71f5fb60c8832fdaf7561d13c77
Scarlet Rose <70824102+scarletquasar@users.noreply.github.com>
2024-01-12 02:55:59 -0300

    chore(ec-service): normalized getNextFiles and made it alter the index.js file to update the index counter

--- apps/ec-service/services/csv-service.ts
+++ apps/ec-service/services/csv-service.ts
@@ -1,4 +1,4 @@
-import { readFile } from 'fs/promises';
+import { readFile, writeFile } from 'fs/promises';
 import { join } from 'path';
 
 type CsvFile = {
@@ -16,27 +16,7 @@ type IndexOptions = {
 };
 
 class CsvService {
-    /**
-     *
-     *
-     * @public
-     * @param {number} quantity
-     * - The quantity of files to be fetched by that function.
-     * @return {Promise<{
-     *         files: CsvFile[],
-     *         end: number
-     *     }>}
-     * - The 'end' variable of the return may be processed to update
-     * the 'currentIndex' value of the index.json. With that, the
-     * 'getNextFiles' function can get in track of the next needed files
-     * to properly lazy load the content from storage and keep consistent
-     * with the results.
-     * @memberof CsvService
-     */
-    public async getNextFiles(quantity: number): Promise<{
-        files: CsvFile[],
-        end: number
-    }> {
+    public static async getNextFiles(quantity: number): Promise<CsvFile[]> {
         const indexPath = join('.store', 'index.json');
         const index = (await readFile(indexPath)).toString();
         const options = JSON.parse(index) as IndexOptions;
@@ -46,20 +26,20 @@ class CsvService {
         
         const pickedFiles = options.files.slice(start, end);
         const readPromises = pickedFiles
-            .map(file => this.readFileWithMetadata(file.path, file.position));
+            .map(file => CsvService.readFileWithMetadata(file.path, file.position));
 
         const files = await Promise.all(readPromises);
         
-        return { files, end };
-        
+        const newOptions: IndexOptions = { ...options, currentIndex: end + 1 };
+        await writeFile(indexPath, JSON.stringify(newOptions));
+
+        return files;
 }
 
-    private async readFileWithMetadata(path: string, position: number) {
+    private static async readFileWithMetadata(path: string, position: number) {
         const content = await readFile(path);
         return { content, path, position }
     }
-
-    private normalizeCsv(encodedCsv: string): string {
-        return 'data:text/csv;charset=utf-8,%EF%BB%BF';
-    }
 }
+
+export { CsvService }
